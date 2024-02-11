3562be370510a40a6af4ec98fdad9d06d0fc4946
Mingxi Cao <mxcao_alina@outlook.com>
2024-02-10 22:17:47 -0800

    a

--- dal.js
+++ dal.js
@@ -47,21 +47,39 @@ async function findOneByGoogleId(googleId) {
 
 async function update(email, amount) {
     const collection = getDb().collection('users');
-    try {
-        const result = await collection.findOneAndUpdate(
-            { email: email },
-            { $inc: { balance: amount } }, // Increment (or decrement for withdrawals) the balance
-            { returnDocument: 'after' } // Ensure the updated document is returned
-        );
-        if (result.value) {
-            return { success: true, value: result.value }; // Return a success response with the updated document
-        } else {
+    collection.findOneAndUpdate(
+        { email: email },
+        { $inc: { balance: amount } }, // Increment (or decrement for withdrawals) the balance
+        { returnDocument: 'after' } // Ensure the updated document is returned
+    ).then(result => {
+        if (result) {
+            return { success: true, value: result.value };
+        }
+        else {
             return { success: false, message: 'Update failed or account not found' };
         }
-    } catch (error) {
-        console.error('Update operation failed:', error);
-        return { success: false, message: 'Internal server error' };
-    }
+    })
+    .catch(err => console.error(`Failed to find and update document: ${err}`))
+
+
+
+    
+    // try {
+    //     const result = await collection.findOneAndUpdate(
+    //         { email: email },
+    //         { $inc: { balance: amount } }, // Increment (or decrement for withdrawals) the balance
+    //         { returnDocument: 'after' } // Ensure the updated document is returned
+    //     );
+    //     console.log(result);
+    //     if (result.value) {
+    //         return { success: true, value: result.value }; // Return a success response with the updated document
+    //     } else {
+    //         return { success: false, message: 'Update failed or account not found' };
+    //     }
+    // } catch (error) {
+    //     console.error('Update operation failed:', error);
+    //     return { success: false, message: 'Internal server error' };
+    // }
 }
 
 async function all() {
